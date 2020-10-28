1dd0c54f45c3b238fd30acceafa2b9e0e99113dc
Sergio Soliz <Sergio Soliz@192.168.124.1>
2020-10-28 18:20:23 -0400

    Agregar clase conexion

---
+++ Plataform/src/plataform/Conexion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plataform;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Sergio Soliz
+ */
+public class Conexion {
+        public static Connection getConexion(){
+        String conexionUrl = "jdbc:sqlserver://sisinf.database.windows.net:1433;"
+                                        + "database=Usuarios;"
+                                        +"user = proyecto;"
+                                        +"password = SistemasdeInformacion2;"
+                                        +"loginTimeout = 30;";
+        try {
+            Connection connection = DriverManager.getConnection(conexionUrl);
+            System.out.println ("Conectado con exito");
+            return connection;
+        }catch (SQLException ex){
+            System.out.println(ex.toString());
+            System.out.println ("Conectado sin exito");
+            return null;
+        }
+    }
+}
